174f0eec68e79b7a391133caa2f3eeaa38bc1223
Bruno Moreira <brunomcmoreira99@gmail.com>
2025-10-13 21:54:40 +0100

    Edit Bootstrapper
    The Main Menu scene is now being loaded by the DefaultScene component from FishNet so we no longer need to load the Main Menu in the Bootstrapper

--- Assets/Scripts/SceneManagement/Bootstrapper.cs
+++ Assets/Scripts/SceneManagement/Bootstrapper.cs
@@ -8,16 +8,12 @@ namespace RooseLabs.SceneManagement
     {
         private void Start()
         {
-            _ = LoadMainMenuScene();
+            _ = Boot();
         }
 
-        private async Task LoadMainMenuScene()
+        private async Task Boot()
         {
-            await SceneManager.LoadSceneAsync("PersistentManagers", LoadSceneMode.Additive);
-            // PersistentManagers currently has a single GameObject with a DontDestroyOnLoad script, so it's safe to
-            // unload it right away since the GameObjects within it will persist anyway.
-            // This will need to change to LoadSceneMode.Additive if for some reason we change that setup.
-            await SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+            await SceneManager.LoadSceneAsync("PersistentManagers", LoadSceneMode.Single);
         }
     }
 }
